ae48da105dd853f5ab0d068e220abd20ef331958
Phạm Hiếu <phamhieukmen199999@gmail.com>
2023-03-31 16:28:33 +0700

    HieuPT41 - submit homework

---
+++ PaperStrip/PaperStrip/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class PaperStrip
+{
+    public static int MinPieces(int[] original, int[] desired)
+    {
+        int count = 1, id = 0;
+        for(int i = 0; i < desired.Length; i++)
+        {
+            if(i == (desired.Length - 1) && id < (desired.Length - 1))
+            {
+                count++;
+                break;
+            }
+            if (original[id] == desired[i])
+            {
+                if (i - 1 >= 0 && id - 1 >= 0)
+                {
+                    if (original[id] == desired[i] && original[id - 1] != desired[i - 1])
+                    {
+                        count++;
+                    }
+                }
+                id++;
+                continue;
+            }
+        }
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] original = new int[] { 1, 4, 3, 2, 5, 6};
+        int[] desired = new int[] { 1, 4, 3, 2, 6, 5, 3 };
+        Console.WriteLine(PaperStrip.MinPieces(original, desired));
+    }
+}
