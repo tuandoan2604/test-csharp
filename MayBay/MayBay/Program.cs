f93c12fc84ece48de42878504484c8220f1fa7ea
Phạm Hiếu <phamhieukmen199999@gmail.com>
2023-04-02 11:47:36 +0700

    hieupt41 - nop bai tap C#

---
+++ MayBay/MayBay/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FlightConnections
+{
+    public static int[] dd = new int[1000]; 
+    public static int dem = 1, dem2 = 0;
+    public static void BFS(bool[,] a, int n, int i)
+    {
+        dd[i] = 1; 
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i, j] != false && dd[j] == 0)
+            {
+                dem++;
+                BFS(a, n, j); 
+            }
+        }
+    }
+    public static int GetMinimumConnections(bool[,] matrix)
+    {
+        BFS(matrix, matrix.GetLength(0), 0);
+        if(dem == matrix.GetLength(0))
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                if (dd[i] == 0) 
+                {
+                    BFS(matrix, matrix.GetLength(0), i); ; 
+                    dem2++;
+                }
+            }
+            return dem2--;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        bool[,] matrix = new bool[,]
+        {
+            {false, true, false, false, true},
+            {true, false, false, false, false},
+            {false, false, false, true, false},
+            {false, false, true, false, false},
+            {true, false, false, false, false}
+        };
+        Console.WriteLine(GetMinimumConnections(matrix)); 
+    }
+}
