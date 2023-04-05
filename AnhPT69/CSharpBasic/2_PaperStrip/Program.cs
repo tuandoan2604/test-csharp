09b208b02756718802e4da9570593cfe8e99e377
anhphtuann <anh.phtuann@gmail.com>
2023-04-05 16:41:01 +0700

    Fix PaperStrip

--- AnhPT69/CSharpBasic/2_PaperStrip/Program.cs
+++ AnhPT69/CSharpBasic/2_PaperStrip/Program.cs
@@ -1,47 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-using System.Runtime.Serialization;
-
-int[] original = new int[] { 1, 4, 3, 2, 5, 6};
-int[] desired = new int[] { 1, 2, 5, 4, 3, 6};
-
-Console.WriteLine(PaperStrip.MinPieces(original, desired));
-
+﻿using System;
 public class PaperStrip
 {
     public static int MinPieces(int[] original, int[] desired)
     {
-        int i = 0, j = 0;
+        int i = 0;
         int n = original.Length;
-        int m = n;
-        List<string> pieces = new List<string>();
+        int pieces = 0;
+        int[] pos = Enumerable.Repeat(-1, n).ToArray();
 
         for (i = 0; i < n; i++)
         {
-            for (j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
-                string str = "";
-                if (original[i] == desired[j])
+                if (pos[j] == -1)
                 {
-                    do
+                    if (original[i] == desired[j])
                     {
-                        str += original[i].ToString();
-                        i++;
-                        j++;
+                        do
+                        {
+                            pos[j] = 1;
+                            i++;
+                            j++;
+                        }
+                        while ((i < n) && (j < n) && (original[i] == desired[j]));
+
+                        i--;
+                        pieces++;
+                        break;
                     }
-                    while ((i < n) && (j < n) && (original[i] == desired[j]));
-
-                    i--;
-                    pieces.Add(str);
-                    break;
                 }
-                
-            }
-        }        
-        return pieces.Count;
 
+            }
+        }
+        return pieces;
     }
-    
-    
+
+    public static void Main(string[] args)
+    {
+        int[] original = new int[] { 1, 4, 3, 2, 5, 6 };
+        int[] desired = new int[] { 1, 2, 5, 4, 3, 6 };
+        Console.WriteLine(PaperStrip.MinPieces(original, desired));
+    }
 }
+
